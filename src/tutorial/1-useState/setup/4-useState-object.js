79dec50e337cb8f3b2519c50268c3593e51dd442
Mark Talevski <mark.talevski+git@gmail.com>
2021-03-19 10:24:06 -0400

    useState destructuring objects

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,34 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: 'Mark',
+    age: 32,
+    message: 'Random message'
+  })
+  const { name, age, message } = person;
+
+  const changeMessage = () => {
+    setPerson(
+      {
+        ...person, 
+        message: 'OH HAI MARK!'
+      }
+    )
+  }
+
+  return(
+    <>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
+      <button className='btn'
+        onClick={changeMessage}
+      >
+        Change Message
+      </button>
+    </>
+  ) 
 };
 
 export default UseStateObject;
